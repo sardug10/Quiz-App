71b21a234f9e407a2c48e4b4c4b70eb4b00294bb
Sarthak Duggal <52778617+sardug10@users.noreply.github.com>
2020-10-02 13:08:34 +0530

    Add files via upload

--- controller/authenticationController.js
+++ controller/authenticationController.js
@@ -1,14 +1,22 @@
+//jshint esversion:8
 const jwt = require("jsonwebtoken");
 const { promisify } = require("util");
 const User = require("../models/userSchema");
+const OUser = require("../models/oauthUsers");
 const catchAsync = require("../utils/catchAsync");
 const AppError = require("../utils/appError");
 const { json } = require("body-parser");
 
 const sendToken = (id, res) => {
-  const token = jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
-  });
+  const token = jwt.sign(
+    {
+      id,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: process.env.JWT_EXPIRES_IN,
+    }
+  );
 
   res.cookie("jwt", token, {
     expiresIn: new Date(
@@ -20,19 +28,57 @@ const sendToken = (id, res) => {
   return token;
 };
 
+exports.oauthSignUp = catchAsync(async (req, res, next) => {
+  console.log(req.body);
+  const newOuser = await OUser.create(req.body);
+  const token = sendToken(newOuser._id, res);
+  // res.status(201).json({
+  //   status: "success",
+  //   message: "Signed-up successfully",
+  //   token,
+  //   user: newOuser,
+  // });
+  res.redirect("/");
+  next();
+});
+
 exports.signUp = catchAsync(async (req, res, next) => {
+  console.log(req.body);
+  console.log("Signup Working");
   const newUser = await User.create(req.body);
 
   const token = sendToken(newUser._id, res);
 
-  res.status(201).json({
-    status: "success",
-    message: "Signed-in successfully",
-    token,
-    user: newUser,
-  });
+  // res.status(201).json({
+  //   status: "success",
+  //   message: "Signed-in successfully",
+  //   token,
+  //   user: newUser,
+  // });
+
+  res.redirect("/");
 
   next();
+});
+
+exports.oauthLogin = catchAsync(async (req, res, next) => {
+  const { _id, userName } = req.body;
+  const ouser = await OUser.findOne({
+    _id,
+  });
+  console.log(ouser);
+  if (ouser == null) {
+    return next(new AppError("You haven't Signed Up Yet", 401));
+  } else {
+    const token = sendToken(ouser._id, res);
+    // res.status(200).json({
+    //   status: "success",
+    //   message: "Logged-in successful",
+    //   token,
+    //   data: ouser,
+    // });
+    res.redirect("/");
+  }
 });
 
 exports.login = catchAsync(async (req, res, next) => {
@@ -44,7 +90,9 @@ exports.login = catchAsync(async (req, res, next) => {
     return next(new AppError("Please provide email and password", 400));
 
   //check if the user exists
-  const user = await User.findOne({ email }).select("+password");
+  const user = await User.findOne({
+    email,
+  }).select("+password");
   //   console.log(user);
   let result = await user.correctPassword(password, user.password);
   //   console.log(result);
@@ -58,19 +106,23 @@ exports.login = catchAsync(async (req, res, next) => {
 
   user.password = undefined;
 
-  res.status(200).json({
-    status: "success",
-    message: "Logged-in successful",
-    token,
-    data: user,
-  });
+  // res.status(200).json({
+  //   status: "success",
+  //   message: "Logged-in successful",
+  //   token,
+  //   data: user,
+  // });
+
+  res.redirect("/");
 });
 
 exports.logout = catchAsync(async (req, res, next) => {
-  res.cookie("jwt", "logout", {
-    expiresIn: new Date(Date.now() + 10 * 1000),
-    httpOnly: true,
-  });
+  // res.cookie("jwt", "logout", {
+  //   expiresIn: new Date(Date.now() + 10 * 1000),
+  //   httpOnly: true,
+  // });
+
+  res.clearCookie("jwt");
 
   res.status(200).json({
     status: "success",
--- public/js/getQuestions.js
+++ public/js/getQuestions.js
@@ -1,15 +1,1 @@
 import axios from "axios";
-
-export const getQuestion = async () => {
-  try {
-    const result = await axios({
-      method: "GET",
-      url: "/api/v1/ques",
-    });
-    // console.log(result);
-    const resultData = result.data.data.data;
-    return resultData;
-  } catch (error) {
-    console.log(error);
-  }
-};
